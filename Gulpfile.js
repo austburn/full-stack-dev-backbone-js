240224bcade165ee0c47fc966fcf65d451ce5edb
austburn <austburn@gmail.com>
2014-11-10 14:02:06 -0500

    Move to gulp.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 var gulp, gutil, source, browserify, server, gutil, source,
-  watchify;
+  watchify, jscs, jshint, stylish, http, express, app, url,
+  proxy, logger;
 
 gulp = require('gulp');
 gutil = require('gutil');
@@ -7,10 +8,33 @@ source = require('vinyl-source-stream');
 browserify = require('browserify');
 server = require('pushstate-server');
 watchify = require('watchify');
+jscs = require('gulp-jscs');
+jshint = require('gulp-jshint');
+stylish = require('jshint-stylish');
+http = require('http');
+express = require('express');
+app = express();
+url = require('url');
+proxy = require('proxy-middleware');
+logger = require('morgan');
 
 gulp.task('default', ['serve']);
 
-gulp.task('watch', function() {
+gulp.task('lint', ['jscs', 'jshint']);
+
+gulp.task('jscs', function () {
+  gulp.src('app/*')
+    .pipe(jscs());
+});
+
+gulp.task('jshint', function () {
+  return gulp.src('./app/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('watch', ['lint'], function() {
   var bundler = watchify(browserify('./app/main.js', watchify.args));
 
   bundler.on('update', rebundle);
@@ -30,5 +54,18 @@ gulp.task('serve', ['watch'], function () {
   server.start({
     port: 7111,
     directory: './static'
+  });
+});
+
+gulp.task('server', ['watch'], function () {
+  app.use(logger({ immediate: true, format: 'dev' }));
+  app.use('/api', proxy(url.parse('http://0.0.0.0:7001/api/')));
+
+  app.use(express.static(__dirname + '/static'));
+
+
+  var port = 7000;
+  http.createServer(app).listen(port, function() {
+    console.log('Frontend listening at %s', port);
   });
 });
--- server/psServer.js
+++
@@ -1,8 +0,0 @@
-var server;
-
-server = require('pushstate-server');
-
-server.start({
-  port: 7111,
-  directory: './static'
-});
